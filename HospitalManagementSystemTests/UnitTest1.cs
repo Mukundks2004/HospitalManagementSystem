f14f1a396fdf2b435356ce9bb158da6f1963bd1a
Mukund <mukundsrinivasan0@gmail.com>
2024-07-30 01:43:19 +1000

    added test csproj

---
+++ HospitalManagementSystemTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using HospitalManagementSystem;
+using NUnit;
+
+namespace HospitalManagementSystemTests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
